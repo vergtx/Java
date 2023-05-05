9cf9f8d5248dd4d2c52950f2de549e01946d48ab
vergtx <130118633+vergtx@users.noreply.github.com>
2023-05-05 17:37:53 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,22 +1,27 @@
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		// 반복이 몇회 해야하는지 모르면 while을 사용한다.
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		while (true) {
-
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-
-			if (a == 0 & b == 0)
-				break;
-
-			System.out.println(a + b);
-
+			String s = br.readLine();
+			int a = Integer.parseInt(s.split(" ")[0]);
+			int b = Integer.parseInt(s.split(" ")[1]);
+			
+			if(a==0 &&b ==0) break;
+			System.out.println(a+b);
+			
 		}
 	}
 
+	
 }
