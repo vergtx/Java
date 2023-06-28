5eff336828b569f7cf13e43bdaf2ec6fee755e52
K <130118633+vergtx@users.noreply.github.com>
2023-06-28 19:54:57 +0900

    [Bronze I] Title: 단어 공부, Time: 632 ms, Memory: 47628 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,53 @@
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+	public static String findMostFrequentAlphabet(String word) {
+		
+		Map<Character, Integer> counter = new HashMap<>();
+		
+		for(int i = 0 ; i < word.length(); i++) {
+			char c =  Character.toLowerCase(word.charAt(i));
+			int count = counter.getOrDefault(c, 0);
+			counter.put(c, count + 1);
+		}
+		
+		int maxcount = 0;
+		char mostFrequent = '?';
+		boolean multipleFrequent = false;
+		
+		for(Map.Entry<Character, Integer> entry : counter.entrySet()) {
+			char c = entry.getKey();
+			int count = entry.getValue();
+			
+			if(count > maxcount) {
+				maxcount = count;
+				mostFrequent = c ;
+				multipleFrequent = false;
+			}
+			else if(count == maxcount) {
+				multipleFrequent = true;
+			}
+		} if(multipleFrequent) {
+			return "?";
+		} else {
+			return Character.toUpperCase(mostFrequent) + "";
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in) ;
+		
+		String word = sc.nextLine();
+		
+		String mostFrequent = findMostFrequentAlphabet(word) ;
+		
+		System.out.println(mostFrequent);
+		
+	}
+
+}
